48613afbc7e9601464812a31bb02de775d7efe1d
Bob Forgey <rforgey@grumpydogconsulting.com>
2022-09-16 21:30:06 -0700

    Allowing multiple NICs per host.

--- scripts/get_all_ips.sh
+++ scripts/get_all_ips.sh
@@ -1,8 +1,18 @@
 
 
 here=$(readlink -f $(dirname $0))
+ips=$(ansible-inventory -i inventory.yml all --list | jq '._meta.hostvars.localhost.ip_start' | tr -d '"').
+domain=$(ansible-inventory -i inventory.yml all --list | jq '._meta.hostvars.localhost.domain' | tr -d '"')
 for mach in $($here/get_all_relevant_machines.sh)
 do
-    guff=$(ansible-inventory -i inventory.yml all --list | jq "._meta.hostvars.${mach}.ip_start, ._meta.hostvars.${mach}.ipa" | tr -d '""' | sed 'N;s/\n/./')
-    echo ${mach}: $guff
+    guff=$(ansible-inventory -i inventory.yml all --list | jq "._meta.hostvars.${mach}.ipa | map(.ip) | map(\"$ips\" + .)|@sh" | tr -d "'\"\"'")
+
+    for ipa in ${guff}
+    do
+        echo $ipa:  ${mach} ${mach}.${domain}
+    done
 done
+
+# jq "._meta.hostvars.${mach}.ip_start, ._meta.hostvars.${mach}.ipa" | tr -d '""' | sed 'N;s/\n/./')
+#     echo ${mach}: $guff
+# done
