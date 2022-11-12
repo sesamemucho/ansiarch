487914c721f68f47caca9480ce3335382f8e8661
Bob Forgey <rforgey@grumpydogconsulting.com>
2022-11-11 21:55:55 -0800

    Starting to bring in x86_64 machines.

--- scripts/get_base_host.sh
+++ scripts/get_base_host.sh
@@ -12,20 +12,22 @@ base_wireless_addr=$($here/get_ansible_var.sh base_wireless_addr localhost)
 wired_host=${ip_start}.${base_wired_addr}
 wireless_host=${ip_start}.${base_wireless_addr}
 
+remote_host=$($here/get_ansible_var.sh remote_addr localhost)
+
 # echo wired is $wired_host
 # echo wireless is $wireless_host
 
-if ping -q -c 2 $wired_host >& /dev/null;
+if ping -q -t 1 -c 2 $wired_host >& /dev/null;
 then
     echo wired_base
+elif ping -q -t 1 -c 2 $wireless_host >& /dev/null;
+then
+    echo wireless_base
+elif ping -q -t 1 -c 2 $remote_host >& /dev/null;
+then
+    echo remote_base
 else
-    if ping -q -c 2 $wireless_host >& /dev/null;
-    then
-        echo wireless_base
-    else
-        echo get_base_hosts.sh: Error - Neither wired_host nor wireless_host found 1>&2
-        echo no-valid-base-host-found
-        exit 1
-    fi
+    echo get_base_hosts.sh: Error - Neither wired_host nor wireless_host found 1>&2
+    echo no-valid-base-host-found
+    exit 1
 fi
-
